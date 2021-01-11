43cf82980b8d259a71324789be9f5b6416256968
prisille <kay@prisille.de>
2021-01-11 11:26:06 +0100

    No need to count down...

--- queens.py
+++ queens.py
@@ -3,21 +3,23 @@
 
 from __future__ import print_function
 
+
 def check_board(i):
-    for j in range(i-1, 0, -1):
+    for j in range(0, i):
         if (d[i-1] == d[j-1]) or (abs(d[i-1]-d[j-1]) == i-j):
             return False
     return True
 
+
 def print_board():
     print("--------")
 
-    for i in range(1,9):
-        print(d[i-1], end ='')
+    for i in range(1, 9):
+        print(d[i-1], end='')
     print()
 
-    for i in range(1,9):
-        for j in range(1,9):
+    for i in range(1, 9):
+        for j in range(1, 9):
             if (d[j-1] == i) and d[j-1] != 0:
                 print("X", end='')
             else:
@@ -26,7 +28,7 @@ def print_board():
 
 
 n = 0
-d = [0,0,0,0,0,0,0,0]
+d = [0, 0, 0, 0, 0, 0, 0, 0]
 count = 0
 
 while True:
@@ -36,8 +38,8 @@ while True:
             break
         n = n-1
     else:
-        if check_board(n+1): 
-            if (n==7):
+        if check_board(n+1):
+            if (n == 7):
                 count = count+1
                 print("Solution = %d" % count)
                 print_board()
